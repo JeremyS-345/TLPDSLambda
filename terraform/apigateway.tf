1a88d0ffd1f7b2bbf509192e5015b2a533fc450f
Jeremy <thepoweroverwhelming@gmail.com>
2024-01-23 01:48:34 -0500

    fix api gateway

--- terraform/apigateway.tf
+++ terraform/apigateway.tf
@@ -1,30 +1,60 @@
-resource "aws_api_gateway_rest_api" "api" {
- name = "api-gateway"
- description = "Proxy to handle requests to our API"
-}
+resource "aws_api_gateway_rest_api" "my_api" {
+  name = "my-api"
+  description = "My API Gateway"
 
-resource "aws_api_gateway_resource" "resource" {
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
-  path_part   = "{proxy+}"
-}
-resource "aws_api_gateway_method" "method" {
-  rest_api_id   = aws_api_gateway_rest_api.api.id
-  resource_id   = aws_api_gateway_resource.resource.id
-  http_method   = "ANY"
-  authorization = "NONE"
-  request_parameters = {
-    "method.request.path.proxy" = true
+  endpoint_configuration {
+    types = ["REGIONAL"]
   }
 }
 
+resource "aws_api_gateway_resource" "root" {
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  parent_id = aws_api_gateway_rest_api.my_api.root_resource_id
+  path_part = "lpds"
+}
+
+resource "aws_api_gateway_method" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  resource_id = aws_api_gateway_resource.root.id
+  http_method = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method_response" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  resource_id = aws_api_gateway_resource.root.id
+  http_method = aws_api_gateway_method.proxy.http_method
+  status_code = "200"
+}
+
+resource "aws_api_gateway_integration_response" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  resource_id = aws_api_gateway_resource.root.id
+  http_method = aws_api_gateway_method.proxy.http_method
+  status_code = aws_api_gateway_method_response.proxy.status_code
+
+  depends_on = [
+    aws_api_gateway_method.proxy,
+    aws_api_gateway_integration.lambda_integration
+  ]
+}
+
+resource "aws_api_gateway_deployment" "deployment" {
+  depends_on = [
+    aws_api_gateway_integration.lambda_integration,
+    #aws_api_gateway_integration.options_integration, # Add this line
+  ]
+
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  stage_name = "dev"
+}
 
 resource "aws_api_gateway_integration" "lambda_integration" {
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  resource_id = aws_api_gateway_resource.resource.id
-  http_method = aws_api_gateway_method.method.http_method
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  resource_id = aws_api_gateway_resource.root.id
+  http_method = aws_api_gateway_method.proxy.http_method
   integration_http_method = "ANY"
-  type = "AWS"
+  type = "AWS_PROXY"
   uri = aws_lambda_function.LPDS_lambda.invoke_arn
 }
 
@@ -39,5 +69,5 @@ resource "aws_lambda_permission" "apigw_lambda" {
   function_name = aws_lambda_function.LPDS_lambda.function_name
   principal = "apigateway.amazonaws.com"
 
-  source_arn = "${aws_api_gateway_rest_api.api.execution_arn}/*/*/*"
+  source_arn = "${aws_api_gateway_rest_api.my_api.execution_arn}/*/*/*"
 }
